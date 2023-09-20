14d75e7a6849fb99ef489bde9f7e074d89b0754d
Trinhvanhan <han.trinhthbd082k3@hcmut.edu.vn>
2023-09-20 14:20:17 +0700

    install library + set routers & controllers

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+const path = require('path')
 const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
 const app = express()
+const route = require('./routes/index')
 const port = 3000
-app.listen(port, () => console.log(`Example app listening at https://localhost:${port}`))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+route(app)
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
